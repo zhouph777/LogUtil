67f89de5973b88c90d92aa89b012c61a48a31f27
zhou.penghui <zhou516371383@163.com>
2021-03-02 18:13:46 +0800

    开启线程保存log文件

--- Logutil/src/main/java/com/util/logutil/LogUtil.java
+++ Logutil/src/main/java/com/util/logutil/LogUtil.java
@@ -11,36 +11,63 @@ import android.view.View;
  * date 2021/2/26
  */
 public class LogUtil {
-    private static String isPrintLog;   //是否开启log输出
-    private static String LogTAG = "LogUtil"; //log输出TAG
+    private  String isPrintLog;   //是否开启log输出
+    private  String LogTAG = "LogUtil"; //log输出TAG
+    private boolean isSaveLog = false;  //是否保存log
 
+    /**
+     * 单例模式（静态内部类）
+     */
+    private static class LazyHolder {
+        private static final LogUtil INSTANCE = new LogUtil();
+    }
+    private LogUtil (){}
+    public static final LogUtil getInstance() {
+        return LazyHolder.INSTANCE;
+    }
 
-    public static void i(String msg) {
+    /**
+     * 初始话LogUtil
+     * @param view 控制日志开启得View
+     * @param isSaveLog 是否保存日志
+     */
+    public void initLogUtil(View view, boolean isSaveLog) {
+        setPrintLog(view);
+        this.isSaveLog = isSaveLog;
+    }
+
+    public void i(String msg) {
         if (isPrintLog.equals("true")) {
             Log.i(LogTAG, msg);
         }
     }
 
-    public static void d(String msg) {
+    public void i(Context context,String msg) {
+        if (isPrintLog.equals("true")) {
+            Log.i(LogTAG, msg);
+        }
+    }
+
+    public  void d(String msg) {
         if (isPrintLog.equals("true")) {
             Log.d(LogTAG, msg);
         }
     }
 
-    public static void e(String msg) {
+    public  void e(String msg) {
         if (isPrintLog.equals("true")) {
             Log.e(LogTAG, msg);
         }
     }
 
-    public static void w(String msg) {
+    public  void w(String msg) {
         if (isPrintLog.equals("true")) {
             Log.w(LogTAG, msg);
         }
     }
 
 
-    public static void v(String msg) {
+    public  void v(String msg) {
         if (isPrintLog.equals("true")) {
             Log.v(LogTAG, msg);
         }
@@ -56,21 +83,21 @@ public class LogUtil {
     private final static long DURATION = 1000;// 规定有效时间
     private static long[] mHits = new long[COUNTS];
 
-    public static void setPrintLog(View view) {
+    public void setPrintLog(View view) {
         view.setOnClickListener(v -> continuousClick(COUNTS, DURATION));
     }
 
     /**
      * 首先我们点击的时候都将数组向左移动一位，将时间赋值给最后一位，从上面的代码中我们可以看出当我们点击了四次，那么最后一位就已经被移到了第一位，然后我们比较时间：
-     *
+     * <p>
      * mHits[0] >= (SystemClock.uptimeMillis() - DURATION)
-     *
+     * <p>
      * 如果是在我们规定的时间内，那么就生效，执行我们所要的操作。
      *
      * @param count 点击次数
      * @param time  点击总时长
      */
-    private static void continuousClick(int count, long time) {
+    private  void continuousClick(int count, long time) {
         //每次点击时，数组向前移动一位
         System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
         //为数组最后一位赋值 系统启动时间
@@ -80,15 +107,16 @@ public class LogUtil {
 
             System.setProperty("persist.danny.log", "true");
             isPrintLog = System.getProperty("persist.danny.log");//将开关设置为系统属性
-            Log.i("LogUtil","Log打开");
-
+            Log.i("LogUtil", "Log打开");
 
         }
     }
 
-    public static void saveLog(Context context,String fileName,String fileContent){
-        SaveFile saveFile = new SaveFile();
-        saveFile.saveInFilesDir(context,fileName,fileContent);
+    private void saveLog(Context context, String fileName, String fileContent) {
+        if (isSaveLog) {
+            SaveFile saveFile = new SaveFile();
+            saveFile.saveInFilesDir(context, fileName, fileContent);
+        }
     }
 
 }
--- Logutil/src/main/java/com/util/logutil/SaveFile.java
+++ Logutil/src/main/java/com/util/logutil/SaveFile.java
@@ -4,19 +4,53 @@ import android.content.Context;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class SaveFile {
 
-    public  void saveInFilesDir(Context content, String fileName, String fileContent) {
+    /**
+     * 创建一个线程池
+     * 2个线程
+     * 最大3个线程
+     * 空闲线程等待时间为0
+     * Unit单位
+     * 队列
+     */
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+            2, 3, 0,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingDeque<>(2));
 
-        try {
-            FileOutputStream fos = content.openFileOutput(fileName, Context.MODE_PRIVATE);
-            fos.write(fileContent.getBytes());
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    /**
+     * 将文件保存在应用专属存储区域
+     *
+     * @param content     上下文
+     * @param fileName    文件名
+     * @param fileContent 要保存得内容
+     */
+    public void saveInFilesDir(Context content, String fileName, String fileContent) {
+        threadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                FileOutputStream fos = null;
+                try {
+                    fos = content.openFileOutput(fileName, Context.MODE_PRIVATE);
+                    fos.write(fileContent.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
 
